02128a4ccf1a592cf434d99ad71052f9f8adf025
Federico Krenn <fedekrenn@gmail.com>
2022-04-28 17:41:54 -0300

    Se añade la seccion acerca de

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoApComponent
+    LogoApComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  title = 'Federico Krenn';
   constructor() { }
 
   ngOnInit(): void {
